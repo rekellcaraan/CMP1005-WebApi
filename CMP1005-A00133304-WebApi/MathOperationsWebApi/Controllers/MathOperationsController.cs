af33a32886c48a554b670a6f584d27004682b87e
rekellcaraan <rekell.caraan@gmail.com>
2022-10-10 16:07:03 -0400

    Implemented WebAPI for Math Operations: SUM

---
+++ CMP1005-A00133304-WebApi/MathOperationsWebApi/Controllers/MathOperationsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MathOperationsLib;
+
+namespace MathOperationsWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    // Math Operations Web Api
+    public class MathOperationsController : ControllerBase
+    {
+        [HttpGet]
+        // Get the sum of 2 or more addends
+        public int Sum([FromQuery(Name = "addends[]")] int[] addends)
+        {
+            return MathOperationsLib.MathOperationsLib.Sum(addends);
+        }
+    }
+}
+
